70e8b26b21c5a1d963851504f2f1d95314f28f39
Mário Agostinho <mariojgagostinho@gmail.com>
2023-09-21 15:03:55 +0000

    Added Domain

---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagement.Domain.Common;
+
+namespace HRLeaveManagementDomain
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string DefaultDays { get; set; } = string.Empty;
+    }
+}
---
+++ HRLeaveManagementDomain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace HRLeaveManagementDomain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string DefaultDays { get; set; } = string.Empty;
+    }
+}
