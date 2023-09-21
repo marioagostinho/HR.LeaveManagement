124684e41365d67eb9f64fe97953a8938777815c
Mário Agostinho <mariojgagostinho@gmail.com>
2023-09-21 16:38:49 +0000

    remove old folder

--- HRLeaveManagementDomain/LeaveType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HRLeaveManagementDomain
-{
-    public class LeaveType
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string DefaultDays { get; set; } = string.Empty;
-    }
-}
